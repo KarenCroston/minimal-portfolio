e2823e544e7b7db79a6b2bad4a7a897f8d7a3d0c
Karen Croston <karencroston@hotmail.co.uk>
2023-11-15 20:51:11 +0000

    initial set up of web page

---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function  Intro() {
+    return (
+        <div className="flex items-center
+        justify-center flex-col text-center pt-20
+        pb-6">
+            <h1>Karen Croston</h1>
+                <p>Software Engineer & Web Developer</p>
+                <p>This is my entire bio</p>
+                </div>
+                
+    )
+}
+
+export default Intro;
